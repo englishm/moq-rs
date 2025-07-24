7cd3e2f54e5c8591a3664ebd0452a322e7c9fa81
Mike English <menglish@cloudflare.com>
2025-07-24 15:15:48 +0200

    Rename Latest Object to Largest Object

--- moq-transport/src/message/filter_type.rs
+++ moq-transport/src/message/filter_type.rs
@@ -5,7 +5,7 @@ use crate::coding::{Decode, DecodeError, Encode, EncodeError};
 #[derive(Clone, Debug, PartialEq)]
 pub enum FilterType {
     LatestGroup = 0x1,
-    LatestObject = 0x2,
+    LargestObject = 0x2,
     AbsoluteStart = 0x3,
     AbsoluteRange = 0x4,
 }
@@ -14,7 +14,7 @@ impl Encode for FilterType {
     fn encode<W: bytes::BufMut>(&self, w: &mut W) -> Result<(), EncodeError> {
         match self {
             Self::LatestGroup => (0x1_u64).encode(w),
-            Self::LatestObject => (0x2_u64).encode(w),
+            Self::LargestObject => (0x2_u64).encode(w),
             Self::AbsoluteStart => (0x3_u64).encode(w),
             Self::AbsoluteRange => (0x4_u64).encode(w),
         }
@@ -25,7 +25,7 @@ impl Decode for FilterType {
     fn decode<R: bytes::Buf>(r: &mut R) -> Result<Self, DecodeError> {
         match u64::decode(r)? {
             0x01 => Ok(Self::LatestGroup),
-            0x02 => Ok(Self::LatestObject),
+            0x02 => Ok(Self::LargestObject),
             0x03 => Ok(Self::AbsoluteStart),
             0x04 => Ok(Self::AbsoluteRange),
             _ => Err(DecodeError::InvalidFilterType),
